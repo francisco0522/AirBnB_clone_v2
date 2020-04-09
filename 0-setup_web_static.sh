63d85241daa1aa354e8da59e784db6cf2c9e170b
francisco0522 <pacho0522@gmail.com>
2020-04-08 19:04:54 -0500

    Adding file 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,12 +9,21 @@ echo "PASO 3"
 ufw allow 'Nginx HTTP'
 echo "PASO 4"
 mkdir -p /data/
+echo "PASO 5"
 mkdir -p /data/web_static/
+echo "PASO 6"
 mkdir -p /data/web_static/releases/
+echo "PASO 7"
 mkdir -p /data/web_static/shared/
+echo "PASO 8"
 mkdir -p /data/web_static/releases/test/
+echo "PASO 9"
 echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "PASO 10"
 ln -sfn /data/web_static/current /data/web_static/releases/test/
+echo "PASO 11"
 chown -R ubuntu: /data/
+echo "PASO 12"
 sed -i '/listen 80 default_server;/a rewrite ^/hbnb_static /data/web_static/current/ permanent;' /etc/nginx/sites-available/default
+echo "PASO 13"
 service nginx start
