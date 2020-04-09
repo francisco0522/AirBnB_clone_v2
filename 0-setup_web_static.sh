cda702bbc22a39af4d0ee0ecb0cbf384e0ce61f1
francisco0522 <pacho0522@gmail.com>
2020-04-08 19:37:07 -0500

    Adding file 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,5 +12,5 @@ mkdir -p /data/web_static/releases/test/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu: /data/
-sed -i '/include /etc/nginx/naxsi.rules};/a location /hbnb_static / { alias /data/web_static/current/;} permanent;' /etc/nginx/sites-available/default
+sed -i '/include /etc/nginx/naxsi.rules};/a location /hbnb_static/ { alias /data/web_static/current/;} permanent;' /etc/nginx/sites-available/default
 service nginx start
