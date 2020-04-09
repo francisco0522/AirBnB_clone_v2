b28e1b8616c42b68955178b22ce000e044bbee40
francisco0522 <pacho0522@gmail.com>
2020-04-08 19:00:24 -0500

    Adding file 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
+apt-get update
+apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sfn /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu: /data/
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/current /data/web_static/releases/test/
+chown -R ubuntu: /data/
 sed -i '/listen 80 default_server;/a rewrite ^/hbnb_static /data/web_static/current/ permanent;' /etc/nginx/sites-available/default
-sudo service nginx start
+service nginx start
