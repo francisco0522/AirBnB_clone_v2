94527f8e7a03655c521e663c3d821eec80469afd
francisco0522 <pacho0522@gmail.com>
2020-04-09 23:35:39 -0500

    Adding task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,28 +12,28 @@ def do_deploy(archive_path):
     if isfile(archive_path) is False:
         return False
     try:
-        put(archive_path, '/tmp/')
-        name_file_ext = archive_path.split("/")[1]
-        name_file = name_file_ext.split(".")[0]
-        cmd_mkdir = 'mkdir -p /data/web_static/releases/{}'.format(name_file)
-        run(cmd_mkdir)
-        cmd_uncom = 'tar -xzf /tmp/{}'.format(name_file_ext)
-        cmd_uncom += ' -C /data/web_static/releases/{}'.format(name_file)
-        run(cmd_uncom)
-        cmd_rm_file = 'rm /tmp/{}'.format(name_file_ext)
-        run(cmd_rm_file)
-        cmd_mv = 'mv /data/web_static/releases/'
-        cmd_mv += '{}/web_static/*'.format(name_file)
-        cmd_mv += ' /data/web_static/releases/{}/'.format(name_file)
-        run(cmd_mv)
-        cmd_rm_dir = 'rm -rf /data/web_static/releases/{}'.format(name_file)
-        cmd_rm_dir += '/web_static'
-        run(cmd_rm_dir)
-        run('rm -rf /data/web_static/current')
-        cmd_cre_sym = 'ln -s /data/web_static/releases/{}/'.format(name_file)
-        cmd_cre_sym += ' /data/web_static/current'
-        run(cmd_cre_sym)
-        print("New version deployed!")
-        return True
+        name = archive_path.split("/")[1]
+        fil = archive_path.split("/")[1].split(".")[0]
+        put(archive_path, "/tmp/{}".format(fil))
+        create = "mkdir -p /data/web_static/releases/{}/".format(fil)
+        run(create)
+        uncompress = "tar -xzf /tmp/{} -C ".format(name)
+        uncompress += "/data/web_static/releases/{}/".format(fil)
+        run(uncompress)
+        delete = "rm /tmp/{}".format(name)
+        run(delete)
+        move = "mv /data/web_static/releases/{}".format(fil)
+        move += "/web_static/* "
+        move += "/data/web_static/releases/{}/".format(fil)
+        run(move)
+        deleteFolder = "rm -rf /data/web_static/releases/{}".format(fil)
+        deleteFolder += "/web_static"
+        run(deleteFolder)
+        deleteLink = "rm -rf /data/web_static/current"
+        run(deleteLink)
+        createLink = "ln -s /data/web_static/releases/{}/ ".format(name)
+        createLink += "/data/web_static/current"
+        run(createLink)
     except Exception:
         return False
+    return True
