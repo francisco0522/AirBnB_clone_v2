c81fdd316813eb778ce8a7eb993178467bcf96e7
francisco0522 <pacho0522@gmail.com>
2020-04-09 23:19:19 -0500

    Adding task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"generates a .tgz archive from the contents of the web_static"
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    "generates a .tgz archive"
+    local("mkdir -p versions")
+    timeNow = datetime.now()
+    tgz = "versions/web_static_"
+    tgz += "{}{}{}".format(timeNow.year, timeNow.month, timeNow.day)
+    tgz += "{}{}{}".format(timeNow.hour, timeNow.minute, timeNow.second)
+    tgz += ".tgz"
+    result = "tar -cvzf "
+    result += tgz
+    result +=" web_static"
+    if local(result) == 1:
+        return None
+    return tgz
