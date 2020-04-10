0c5f37f1486b1024aca4bd8e18ef426c393df132
francisco0522 <pacho0522@gmail.com>
2020-04-09 23:44:17 -0500

    Fixing 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 "generates a .tgz archive from the contents of the web_static"
 from fabric.api import local, put, run, env
+from os import path
 from datetime import datetime
-from os.path import isfile
+from shlex import split
 
+env.user = 'ubuntu'
 env.hosts = ["35.243.229.224", "34.229.175.198"]
 
-def do_pack():
-    "generates a .tgz archive"
-    local("mkdir -p versions")
-    timeNow = datetime.now()
-    tgz = "versions/web_static_"
-    tgz += "{}{}{}".format(timeNow.year, timeNow.month, timeNow.day)
-    tgz += "{}{}{}".format(timeNow.hour, timeNow.minute, timeNow.second)
-    tgz += ".tgz"
-    result = "tar -cvzf "
-    result += tgz
-    result += " web_static"
-    if local(result) == 1:
-        return None
-    return tgz
 
 def do_deploy(archive_path):
     "distributes an archive to your web servers"
-    if isfile(archive_path) is False:
+    if not path.exists(archive_path) or (
+                                         path.exists(archive_path) and
+                                         path.isdir(archive_path)):
         return False
     try:
+        put(archive_path, '/tmp/')
         name = archive_path.split("/")[1]
-        fil = archive_path.split("/")[1].split(".")[0]
-        put(archive_path, "/tmp/{}".format(fil))
+        fil = name.split(".")[0]
         create = "mkdir -p /data/web_static/releases/{}/".format(fil)
         run(create)
         uncompress = "tar -xzf /tmp/{} -C ".format(name)
@@ -48,6 +38,6 @@ def do_deploy(archive_path):
         createLink = "ln -s /data/web_static/releases/{}/ ".format(name)
         createLink += "/data/web_static/current"
         run(createLink)
+        return True
     except Exception:
         return False
-    return True
