d4da3c8f5743896c0eb87dfb9c2bca576ff53141
francisco0522 <pacho0522@gmail.com>
2020-04-10 19:16:14 -0500

    Fixing 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-"generates a .tgz archive from the contents of the web_static"
-from fabric.api import local, put, run, env
+"""distributes an archive to your web servers"""
+
+from fabric.api import *
 from os import path
 from datetime import datetime
 from shlex import split
 
-env.user = 'ubuntu'
 env.hosts = ["35.243.229.224", "34.229.175.198"]
 
 
 def do_deploy(archive_path):
-    "distributes an archive to your web servers"
+    """Deplay the archive tgz"""
     if not path.exists(archive_path) or (
                                          path.exists(archive_path) and
                                          path.isdir(archive_path)):
         return False
     try:
         put(archive_path, '/tmp/')
-        name = archive_path.split("/")[1]
-        fil = name.split(".")[0]
-        create = "mkdir -p /data/web_static/releases/{}/".format(fil)
-        run(create)
-        uncompress = "tar -xzf /tmp/{} -C ".format(name)
-        uncompress += "/data/web_static/releases/{}/".format(fil)
-        run(uncompress)
-        delete = "rm /tmp/{}".format(name)
-        run(delete)
-        move = "mv /data/web_static/releases/{}".format(fil)
-        move += "/web_static/* "
-        move += "/data/web_static/releases/{}/".format(fil)
-        run(move)
-        deleteFolder = "rm -rf /data/web_static/releases/{}".format(fil)
-        deleteFolder += "/web_static"
-        run(deleteFolder)
-        deleteLink = "rm -rf /data/web_static/current"
-        run(deleteLink)
-        createLink = "ln -s /data/web_static/releases/{}/ ".format(name)
-        createLink += "/data/web_static/current"
-        run(createLink)
+        name_file_ext = archive_path.split("/")[1]
+        name_file = name_file_ext.split(".")[0]
+        cmd_mkdir = 'mkdir -p /data/web_static/releases/{}'.format(name_file)
+        run(cmd_mkdir)
+        cmd_uncom = 'tar -xzf /tmp/{}'.format(name_file_ext)
+        cmd_uncom += ' -C /data/web_static/releases/{}'.format(name_file)
+        run(cmd_uncom)
+        cmd_rm_file = 'rm /tmp/{}'.format(name_file_ext)
+        run(cmd_rm_file)
+        cmd_mv = 'mv /data/web_static/releases/'
+        cmd_mv += '{}/web_static/*'.format(name_file)
+        cmd_mv += ' /data/web_static/releases/{}/'.format(name_file)
+        run(cmd_mv)
+        cmd_rm_dir = 'rm -rf /data/web_static/releases/{}'.format(name_file)
+        cmd_rm_dir += '/web_static'
+        run(cmd_rm_dir)
+        run('rm -rf /data/web_static/current')
+        cmd_cre_sym = 'ln -s /data/web_static/releases/{}/'.format(name_file)
+        cmd_cre_sym += ' /data/web_static/current'
+        run(cmd_cre_sym)
+        print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
