ffcc330c05d7502e20ae2166c381f9c875f7f9fa
francisco0522 <pacho0522@gmail.com>
2020-04-22 21:39:42 -0500

    return

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,7 @@ class FileStorage:
         """Delete obj olny if it exists"""
         if obj is not None:
             del self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)]
+
+def close(self):
+        """Close session"""
+        reload()
